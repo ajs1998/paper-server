d938c6e0b860f5dcd8ef818b84dce9a84ecdc173
Alex Sweeney <ajs1998@gmail.com>
2022-07-23 20:32:25 +0000

    it works!

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
-docker run -it --rm -v /var/opt/paper-server/data:/data \
-    -e TYPE=PAPER \
-    -e EULA=TRUE \
-    -p 25565:25565 \
-    --name mc itzg/minecraft-server
+docker-compose up -d --rm
